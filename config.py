72588ab83b59dff87a129743a11ed910045e6267
Yashwanth2725 <yashwanthnaidugalla@gmail.com>
2026-08-28 16:34:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,14 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Use a writable database location on the cloud server
-DATABASE_PATH = os.path.join("/tmp", "attendance.db")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+else:
+    DATABASE_URL = "sqlite:///" + os.path.join(BASE_DIR, "database", "attendance.db")
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
