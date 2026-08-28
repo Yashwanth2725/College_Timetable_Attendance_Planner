4cc2a4ae9019df70720235d7451c8b1b0f5670fb
Yashwanth2725 <yashwanthnaidugalla@gmail.com>
2026-08-28 16:18:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+# Use a writable database location on the cloud server
+DATABASE_PATH = os.path.join("/tmp", "attendance.db")
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-        BASE_DIR, "database", "attendance.db"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
